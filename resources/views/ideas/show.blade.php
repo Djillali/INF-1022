a3c5060c22c5649284f7f5b71efa763d5c54f869
Djillali <ch.djillali@gmail.com>
2022-04-05 23:20:24 -0400

    Dialogues dynamiques en utilisant alpine.js

--- resources/views/ideas/show.blade.php
+++ resources/views/ideas/show.blade.php
@@ -38,18 +38,26 @@
                     <div>&bull;</div>
                     <div class="text-gray-900">3 @lang('buttons.comments')</div>
                 </div>
-                <div class="flex items-center space-x-2">
+                <div 
+                	x-data="{ isOpen: false}"
+                	class="flex items-center space-x-2"
+                >
                     <div
                         class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">@lang('ideas.open')</div>
                     <button
-                        class="relative bg-gray-100 hover:bg-gray-200 border rounded-full h-7 transition duration-150 ease-in py-2 px-3">
+                        @click="isOpen = !isOpen"
+                        class="relative bg-gray-100 hover:bg-gray-200 border rounded-full h-7 transition duration-150 ease-in py-2 px-3"
+                    >
                         <svg fill="currentColor" width="24" height="6">
                             <path
                                 d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z"
                                 style="color: rgba(163, 163, 163, .5)">
                         </svg>
-                        <ul
-                            class="hidden absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
+                        <ul                            
+                        	x-cloak x-show.transition.origin.top.left="isOpen" @click.away="isOpen = false"
+                            @keydown.escape.window="isOpen = false"
+                            class="absolute w-44 text-left font-semibold bg-white shadow-md rounded-xl py-3 ml-8"
+                        >
                             <li><a href="#"
                                     class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">@lang('buttons.spam')</a></li>
                             <li><a href="#"
@@ -64,17 +72,114 @@
 
 <div class="buttons-container flex items-center justify-between mt-6">
     <div class="flex items-center space-x-4 ml-6">
-        <button type="button"
-            class="flex items-center justify-center h-11 w-32 text-xs bg-blue-500 text-white font-semibold rounded-xl border border-blue-500 hover:bg-blue-700 transition duration-150 ease-in px-6 py-3">
-            <span class="ml-1">@lang('buttons.reply')</span>
-        </button>
-        <button type="button"
-            class="flex items-center justify-center w-36 h-11 text-xs bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3">
-            <span>@lang('buttons.setStatus')</span>
-            <svg class="w-4 h-4 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-            </svg>
-        </button>
+        <div x-data="{ isOpen: false }" class="relative">
+            <button type="button" @click="isOpen = !isOpen"
+                class="flex items-center justify-center h-11 w-32 text-sm bg-blue-500 text-white font-semibold rounded-xl border border-blue-500 hover:bg-blue-500 transition duration-150 ease-in px-6 py-3">
+                @lang('buttons.reply')
+            </button>
+            <div class="absolute z-10 w-96 text-left font-semibold text-sm bg-white shadow-xl rounded-xl mt-2" x-cloak
+                x-show.transition.origin.top.left="isOpen" @click.away="isOpen = false"
+                @keydown.escape.window="isOpen = false">
+                <form action="#" class="space-y-4 px-4 py-6">
+                    <div>
+                        <textarea name="post_comment" id="post_comment" cols="30" rows="4"
+                            class="w-full text-sm bg-gray-100 rounded-xl placeholder-gray-900 border-none px-4 py-2"
+                            placeholder="Go ahead, don't be shy. Share your thoughts..."></textarea>
+                    </div>
+
+                    <div class="flex items-center space-x-3">
+                        <button type="button"
+                            class="flex items-center justify-center h-11 w-1/2 text-sm bg-blue-500 text-white font-semibold rounded-xl border border-blue-500 hover:bg-blue-500 transition duration-150 ease-in px-6 py-3">
+                            @lang('buttons.postComment')
+                        </button>
+                        <button type="button"
+                            class="flex items-center justify-center w-32 h-11 text-xs bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3">
+                            <svg class="text-gray-600 w-4 transform -rotate-45" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
+                            </svg>
+                            <span class="ml-1">@lang('buttons.attach')</span>
+                        </button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+        <div class="relative" x-data="{ isOpen: false }">
+            <button type="button" @click="isOpen = !isOpen"
+                class="flex items-center justify-center w-36 h-11 text-sm bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3">
+                <span>@lang('buttons.setStatus')</span>
+                <svg class="w-4 h-4 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+                </svg>
+            </button>
+            <div x-cloak x-show.transition.origin.top.left="isOpen" @click.away="isOpen = false"
+                @keydown.escape.window="isOpen = false"
+                class="absolute z-20 w-76 text-left font-semibold text-sm bg-white shadow-xl rounded-xl mt-2">
+                <form action="#" class="space-y-4 px-4 py-6">
+                    <div class="space-y-2">
+                        <div>
+                            <label class="inline-flex items-center">
+                                <input type="radio" class="bg-gray-200 text-gray-600 border-none" name="status"
+                                    value="1" checked>
+                                <span class="ml-2">@lang('ideas.open')</span>
+                            </label>
+                        </div>
+                        <div>
+                            <label class="inline-flex items-center">
+                                <input type="radio" class="bg-gray-200 text-purple-600 border-none" name="status"
+                                    value="2">
+                                <span class="ml-2">@lang('ideas.considering')</span>
+                            </label>
+                        </div>
+                        <div>
+                            <label class="inline-flex items-center">
+                                <input type="radio" class="bg-gray-200 text-yellow-600 border-none" name="status"
+                                    value="3">
+                                <span class="ml-2">@lang('ideas.in_progress')</span>
+                            </label>
+                        </div>
+                        <div>
+                            <label class="inline-flex items-center">
+                                <input type="radio" class="bg-gray-200 text-green-600 border-none" name="status"
+                                    value="3">
+                                <span class="ml-2">@lang('ideas.implemented')</span>
+                            </label>
+                        </div>
+                        <div>
+                            <label class="inline-flex items-center">
+                                <input type="radio" class="bg-gray-200 text-red-600 border-none" name="status"
+                                    value="3">
+                                <span class="ml-2">@lang('ideas.closed')</span>
+                            </label>
+                        </div>
+                    </div>
+
+                    <div>
+                        <textarea name="update_comment" id="update_comments" cols="30" rows="3"
+                            class="w-full text-sm bg-gray-100 rounded-xl placeholder-gray-900 border-none px-4 py-2"
+                            placeholder="Add an update comment (optional)"></textarea>
+                    </div>
+
+                    <div class="flex items-center justify-between space-x-3">
+                        <button type="button"
+                            class="flex items-center justify-center w-1/2 h-11 text-xs bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3">
+                            <svg class="text-gray-600 w-4 transform -rotate-45" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
+                            </svg>
+                            <span class="ml-1">@lang('buttons.attach')</span>
+                        </button>
+                        <button type="submit"
+                            class="flex items-center justify-center w-1/2 h-11 text-xs bg-blue-500 text-white font-semibold rounded-xl border border-blue-500 hover:bg-blue-500 transition duration-150 ease-in px-6 py-3">
+                            <span class="ml-1">@lang('buttons.update')</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 
     <div class="flex items-center space-x-3">
